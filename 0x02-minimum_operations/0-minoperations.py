8e6f2c0f6ed1ed0fa2358f9326488c5212af4171
laribox <laribox2014@gmail.com>
2024-10-17 13:36:03 -0700

    task

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -5,7 +5,10 @@ to get exactly n 'H' characters using only 'Copy All' and 'Paste'.
 """
 
 def minOperations(n):
-    """Calculates the minimum number of operations to achieve n 'H' characters."""
+    """
+        Calculates the minimum number of operations
+        to achieve n 'H' characters.
+    """
     if n <= 1:
         return 0
 
@@ -20,4 +23,3 @@ def minOperations(n):
         divisor += 1
 
     return operations
-
