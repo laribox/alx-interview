2c56f88c24e4116f302f0f1ffaf0757ae86a90c6
laribox <laribox2014@gmail.com>
2024-10-24 15:42:05 +0000

    tasks

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,88 +1,93 @@
 #!/usr/bin/python3
-'''A script for parsing HTTP request logs.
-'''
+"""
+This is the `0-stats` module. It is meant to be a log parser.
+"""
 import re
 
 
-def extract_input(input_line):
-    '''Extracts sections of a line of an HTTP request log.
-    '''
-    fp = (
-        r'\s*(?P<ip>\S+)\s*',
-        r'\s*\[(?P<date>\d+\-\d+\-\d+ \d+:\d+:\d+\.\d+)\]',
-        r'\s*"(?P<request>[^"]*)"\s*',
-        r'\s*(?P<status_code>\S+)',
-        r'\s*(?P<file_size>\d+)'
-    )
-    info = {
-        'status_code': 0,
-        'file_size': 0,
-    }
-    log_fmt = '{}\\-{}{}{}{}\\s*'.format(fp[0], fp[1], fp[2], fp[3], fp[4])
-    resp_match = re.fullmatch(log_fmt, input_line)
-    if resp_match is not None:
-        status_code = resp_match.group('status_code')
-        file_size = int(resp_match.group('file_size'))
-        info['status_code'] = status_code
-        info['file_size'] = file_size
-    return info
+# total_size is the total size of all the files processed.
+total_size = 0
+
+# status_code_dict is a dictionary of the status codes and their count
+# across all files. The status codes are used as keys, and their count values.
+status_code_dict = {
+    200: 0,
+    301: 0,
+    400: 0,
+    401: 0,
+    403: 0,
+    404: 0,
+    405: 0,
+    500: 0,
+}
+
+# pp are the pattern parts.
+pp = (
+    r'^\b(?:\d{1,3}\.){3}\d{1,3}\b',
+    r'\s+-\s+',
+    r'\[\d{4}-\d{2}-\d{2}\s+(?:\d{2}:){2}\d{2}\.\d+\]\s+',
+    r'"GET /projects/260 HTTP/1\.1"\s+',
+    r'(?P<status_code>\d{3})\s+(?P<file_size>\d+)$'
+)
+# line_pattern is the regex pattern to match the lines in the files.
+line_pattern = re.compile(
+    '{}{}{}{}{}'.format(pp[0], pp[1], pp[2], pp[3], pp[4])
+)
 
 
-def print_statistics(total_file_size, status_codes_stats):
-    '''Prints the accumulated statistics of the HTTP request log.
-    '''
-    print('File size: {:d}'.format(total_file_size), flush=True)
-    for status_code in sorted(status_codes_stats.keys()):
-        num = status_codes_stats.get(status_code, 0)
-        if num > 0:
-            print('{:s}: {:d}'.format(status_code, num), flush=True)
+def print_stats() -> None:
+    """
+    print_stats prints the stats.
+    """
+    print("File size: {}".format(total_size), flush=True)
+    for status_code, count in sorted(status_code_dict.items()):
+        if count == 0:
+            continue
+        print("{}: {}".format(status_code, count), flush=True)
 
 
-def update_metrics(line, total_file_size, status_codes_stats):
-    '''Updates the metrics from a given HTTP request log.
-
-    Args:
-        line (str): The line of input from which to retrieve the metrics.
-
-    Returns:
-        int: The new total file size.
-    '''
-    line_info = extract_input(line)
-    status_code = line_info.get('status_code', '0')
-    if status_code in status_codes_stats.keys():
-        status_codes_stats[status_code] += 1
-    return total_file_size + line_info['file_size']
-
-
-def run():
-    '''Starts the log parser.
-    '''
-    line_num = 0
-    total_file_size = 0
-    status_codes_stats = {
-        '200': 0,
-        '301': 0,
-        '400': 0,
-        '401': 0,
-        '403': 0,
-        '404': 0,
-        '405': 0,
-        '500': 0,
-    }
+def compute_stats(line_no: int, line: str) -> None:
+    """
+    compute_stats keeps track of the total size and the count of each
+    status code. It prints the stats after every 10 lines processed.
+    """
     try:
+        match = line_pattern.match(line)
+        if not match:
+            return
+
+        status_code = int(match.group("status_code"))
+        file_size = int(match.group("file_size"))
+        if status_code not in status_code_dict:
+            return
+
+        global total_size
+        total_size += file_size
+
+        status_code_dict[status_code] = status_code_dict.get(
+            status_code, 0) + 1
+
+        if line_no % 10 == 0:
+            print_stats()
+    except ValueError:
+        return
+
+
+def parse_lines() -> None:
+    """
+    parse_lines parses each line in the logs and computes the stats.
+    It prints the stats after every 10 lines processed, or when a
+    KeyboardInterrupt is encountered.
+    """
+    try:
+        line_no = 0
         while True:
             line = input()
-            total_file_size = update_metrics(
-                line,
-                total_file_size,
-                status_codes_stats,
-            )
-            line_num += 1
-            if line_num % 10 == 0:
-                print_statistics(total_file_size, status_codes_stats)
+            line_no += 1
+            compute_stats(line_no, line)
     except (KeyboardInterrupt, EOFError):
-        print_statistics(total_file_size, status_codes_stats)
+        print_stats()
 
 
-if __name__ == '__main__':
-    run()
+if __name__ == "__main__":
+    parse_lines()
