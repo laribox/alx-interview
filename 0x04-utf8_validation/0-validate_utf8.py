290f58a471de2a9059059a45f97f3768b38029e6
laribox <laribox2014@gmail.com>
2024-10-31 20:22:12 +0000

    task 0

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Module for validUtf8 method"""
+
+
+def validUTF8(data):
+    """
+    Method that determines if a given data set represents a valid UTF-8 encoding
+    """
+    n_bytes = 0
+    mask1 = 1 << 7
+    mask2 = 1 << 6
+    for num in data:
+        
+        mask = 1 << 7
+        if n_bytes == 0:
+            while mask & num:
+                n_bytes += 1
+                mask = mask >> 1
+
+            if n_bytes == 0:
+                continue
+
+            if n_bytes == 1 or n_bytes > 4:
+                return False
+        else:
+            if not (num & mask1 and not (num & mask2)):
+                return False
+        n_bytes -= 1
+    return n_bytes == 0
