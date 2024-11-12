f67657af316ad4f44f77b1da22ffcfc987a3e6e9
laribox <laribox2014@gmail.com>
2024-11-12 18:01:52 +0000

    task 0

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,23 +4,27 @@ const request = require('request');
 const movieId = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
-request(url, (error, response, body) => {
+const url = `https://swapi-api.alx-tools.com/api/films/${argv[2]}/`;
+request(url, async function (error, response, body) {
   if (error) {
-    console.error(error);
-    return;
-  }
-
-  const characters = JSON.parse(body).characters;
-  
-  characters.forEach(characterUrl => {
-    request(characterUrl, (err, res, charBody) => {
-      if (err) {
-        console.error(err);
-        return;
+    console.error('error:', error);
+  } else {
+    const characters = JSON.parse(body).characters;
+    for (const character of characters) {
+      try {
+        const res = await new Promise((resolve, reject) => {
+          request(character, (error, response, body) => {
+            if (error) {
+              reject(error);
+            } else {
+              resolve(JSON.parse(body).name);
+            }
+          });
+        });
+        console.log(res);
+      } catch (err) {
+        console.error('error:', err);
       }
-      const characterName = JSON.parse(charBody).name;
-      console.log(characterName);
-    });
-  });
+    }
+  }
 });
-
