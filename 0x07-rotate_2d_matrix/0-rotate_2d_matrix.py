ee3359e493a1f4c97cf807ca39ef7ed58a9b0406
laribox <laribox2014@gmail.com>
2024-11-20 14:20:19 +0000

    task 0

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Rotate a 2D matrix 90 degrees clockwise.
+"""
+
+def rotate_2d_matrix(matrix):
+    """
+    Rotates a 2D matrix 90 degrees clockwise in place.
+
+    Args:
+        matrix (list of list of int): The 2D matrix to rotate.
+
+    Returns:
+        None: The matrix is modified in place.
+    """
+    n = len(matrix)
+
+    # Transpose the matrix
+    for i in range(n):
+        for j in range(i + 1, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    # Reverse each row
+    for row in matrix:
+        row.reverse()
---
+++ 0x07-rotate_2d_matrix/README.md
@@ -0,0 +1,24 @@
+# Rotate 2D Matrix
+
+This project implements a function to rotate a 2D matrix 90 degrees clockwise.
+
+## Requirements
+
+- Python 3.8.10 on Ubuntu 20.04 LTS
+- Code must follow `pycodestyle` guidelines (version 2.8.0).
+- All files must end with a new line and be executable.
+
+## Function Prototype
+
+```python
+def rotate_2d_matrix(matrix):
+    """
+    Rotates a 2D matrix 90 degrees clockwise in place.
+
+    Args:
+        matrix (list of list of int): The 2D matrix to rotate.
+
+    Returns:
+        None: The matrix is modified in place.
+    """
+
