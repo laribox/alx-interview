a58a9c784e5948e875825eaac5256ae55d4911fa
laribox <laribox2014@gmail.com>
2024-12-10 20:50:15 +0000

    task 0

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,12 +2,14 @@
 
 """ Prime Game Algorithm Python """
 
+
 def is_prime(n):
     """ Checks if a number given n is a prime number """
     for i in range(2, int(n ** 0.5) + 1):
         if not n % i:
             return False
     return True
+
 
 def calculate_primes(n, primes):
     """ Calculate all primes """
@@ -18,6 +20,7 @@ def calculate_primes(n, primes):
                 primes.append(i)
             else:
                 primes.append(0)
+
 
 def isWinner(x, nums):
     """
